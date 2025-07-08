3295692eb93c280afff96992bad3a430bee1437b
Cypher Studios <cypherstudio5m@gmail.com>
2025-07-08 20:28:01 +0500

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,47 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'Made By Cypher Studio'
+
+files {
+  'cyphercustomsounds/sfx/resident/ambience.awc',
+  'cyphercustomsounds/sfx/resident/animals_footsteps.awc',
+  'cyphercustomsounds/sfx/resident/collision.awc',
+  'cyphercustomsounds/sfx/resident/collisions.awc',
+  'cyphercustomsounds/sfx/resident/doors.awc',
+  'cyphercustomsounds/sfx/resident/explosions.awc',
+  'cyphercustomsounds/sfx/resident/feet_materials.awc',
+  'cyphercustomsounds/sfx/resident/feet_resident.awc',
+  'cyphercustomsounds/sfx/resident/frontend.awc',
+  'cyphercustomsounds/sfx/resident/low_latency.awc',
+  'cyphercustomsounds/sfx/resident/melee.awc',
+  'cyphercustomsounds/sfx/resident/movement.awc',
+  'cyphercustomsounds/sfx/resident/player_switch.awc',
+  'cyphercustomsounds/sfx/resident/vehicles.awc',
+  'cyphercustomsounds/sfx/resident/weapons.awc',
+  'cyphercustomsounds/sfx/resident/weather.awc',
+  'cyphercustomsounds/sfx/weapons_player/lmg_combat.awc',
+  'cyphercustomsounds/sfx/weapons_player/lmg_mg_player.awc',
+  'cyphercustomsounds/sfx/weapons_player/mgn_sml_am83_vera.awc',
+  'cyphercustomsounds/sfx/weapons_player/mgn_sml_am83_verb.awc',
+  'cyphercustomsounds/sfx/weapons_player/mgn_sml_sc__l.awc',
+  'cyphercustomsounds/sfx/weapons_player/ptl_50cal.awc',
+  'cyphercustomsounds/sfx/weapons_player/ptl_combat.awc',
+  'cyphercustomsounds/sfx/weapons_player/ptl_pistol.awc',
+  'cyphercustomsounds/sfx/weapons_player/ptl_px4.awc',
+  'cyphercustomsounds/sfx/weapons_player/ptl_rubber.awc',
+  'cyphercustomsounds/sfx/weapons_player/sht_bullpup.awc',
+  'cyphercustomsounds/sfx/weapons_player/sht_pump.awc',
+  'cyphercustomsounds/sfx/weapons_player/smg_micro.awc',
+  'cyphercustomsounds/sfx/weapons_player/smg_smg.awc',
+  'cyphercustomsounds/sfx/weapons_player/snp_heavy.awc',
+  'cyphercustomsounds/sfx/weapons_player/snp_rifle.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_grenade_player.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_minigun_player.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_prog_ar_player.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_railgun.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_rpg_player.awc',
+  'cyphercustomsounds/sfx/weapons_player/spl_tank_player.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'cyphercustomsounds/sfx/resident'
+data_file 'AUDIO_WAVEPACK' 'cyphercustomsounds/sfx/weapons_player'
